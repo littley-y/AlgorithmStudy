ef333f19036a13febf1e0088e5c0b2dd1388a2c3
Yeongtae Hyun <hyunyoungtae@gmail.com>
2023-05-16 10:59:08 +0900

    23/05/16 풀이 실패 : 소인수분해 알고리즘 찾아보기

---
+++ 기초/1. 수학/2609_Y.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int A, B;
+vector<int> vecA, vecB;
+
+int next(int i) {
+  if (i == 1 || i == 2)
+    return i + 1;
+}
+
+void solution() {}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> A >> B;
+  return 0;
+}
+
+// 2^3 * 3
+// 2 * 3^2
