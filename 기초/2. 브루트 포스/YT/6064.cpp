f6860955e3bd6848f0c870c35857db68d3fe8e3c
Yeongtae Hyun <hyunyoungtae@gmail.com>
2023-05-22 15:02:52 +0900

    23/05/19 6일차 YT 풀이 수정

--- 기초/2. 브루트 포스/YT/6064.cpp
+++ 기초/2. 브루트 포스/YT/6064.cpp
@@ -1,3 +1,15 @@
+/*
+맨 처음에는 단순 덧셈으로 해결하려고 했고
+그 다음에는 visited 배열 이용해서 이미 탐색한 점을 체크하는 식으로 하려고 했는데
+둘다 시간초과가 떴다.
+풀이를 찾아보니 최소공배수를 이용하는 방식으로 하면 된다.
+
+1. M과 N의 최소공배수를 구한다. 이것이 루프의 최대값이 된다.
+2. i를 x로 고정시켜놓고, i년일때 i를 n으로 나눈 나머지값(ny)을 확인한다.
+3. y가 1부터 시작하기 때문에 ny == 0이면 y는 최대값(N)이라는 의미.
+4. ny == y이면 원하는 y값을 찾았다는 의미이므로 종료.
+*/
+
 #include <cstring>
 #include <iostream>
 
@@ -5,26 +17,27 @@ using namespace std;
 
 int T, M, N, x, y;
 
-void solution() {
-  int curr = 1, a = 1, b = 1;
-  int visited[M][N];
-  memset(visited, 0, sizeof(int) * M * N);
+int gcd(int a, int b) {
+  if (b == 0)
+    return a;
+  return gcd(b, a % b);
+}
 
-  while (!(a == x && b == y)) {
-    if (visited[a - 1][b - 1] == true) {
-      cout << -1 << '\n';
-      return;
+int lcm(int a, int b) { return (a * b) / gcd(a, b); }
+
+void solution() {
+  int ans = -1;
+  int maxI = lcm(M, N);
+  for (int i = x; i <= maxI; i += M) {
+    int ny = i % N;
+    if (ny == 0)
+      ny = N;
+    if (ny == y) {
+      ans = i;
+      break;
     }
-    visited[a - 1][b - 1] = true;
-    if (a == M)
-      a = 0;
-    if (b == N)
-      b = 0;
-    a++;
-    b++;
-    curr++;
   }
-  cout << curr << '\n';
+  cout << ans << '\n';
 }
 
 int main() {
