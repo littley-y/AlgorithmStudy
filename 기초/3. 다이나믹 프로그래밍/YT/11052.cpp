92ba2894fabd04a857ef981d52dfcb586e24105b
Yeongtae Hyun <hyunyoungtae@gmail.com>
2023-06-13 23:20:22 +0900

    23/06/13 20일차 YT 풀이 완료

--- 기초/3. 다이나믹 프로그래밍/YT/11052.cpp
+++ 기초/3. 다이나믹 프로그래밍/YT/11052.cpp
@@ -1,3 +1,15 @@
+/*
+DP는 손으로 직접 쓰면서 해보는게 가장 좋은 방법인듯..
+만약 4개를 살 때 경우의 수를 작성해보면
+
+price[1] + 3개 살떄 최대값
+price[2] + 2개 살때 최대값
+price[3] + 1개 살때 최대값
+price[4];
+
+이렇게 나온다. 이를 토대로 점화식을 세워보자.
+*/
+
 #include <iostream>
 
 using namespace std;
@@ -15,9 +27,13 @@ int main() {
     cin >> price[i];
   }
 
+  for (int i = 1; i <= N; i++) {
+    for (int j = 1; j <= i; j++) {
+      // 바로 전의 계산 값과, 현재의 경우 중 큰 쪽이 현재 값이 된다.
+      arr[i] = max(arr[i], arr[i - j] + price[j]);
+    }
+  }
+  cout << arr[N] << '\n';
+
   return 0;
 }
-
-/*
-
-*/
